1e14695284c1f943cf52e1c3863a20a7fcbb5ae6
Maarten Sap <maarten@sas.upenn.edu>
2014-09-19 11:57:04 -0400

    added __init__ (2)

--- TwitterMySQL/__init__.py
+++ TwitterMySQL/__init__.py
@@ -3,3 +3,10 @@ __author__ = "Maarten Sap"
 __email__ = "maartensap93@gmail.com"
 __version__ = "0.3"
 __copyright__ = "Copyright 2014 Maarten Sap"
+
+try:
+    from .TwitterMySQL import TwitterMySQL
+except:
+    pass
+
+__all__ = ["TwitterMySQL"]
